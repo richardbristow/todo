4c4fbb339089b301717f09dc03f0a32ac9fea492
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-05-07 14:30:09 +0100

    refactored handleChange/submit functions

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,29 +1,24 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
 import React, { useState } from 'react';
 import './css/addItem.css';
 
 const AddItem = ({ addItem }) => {
   const [value, setValue] = useState('');
 
-  const handleChange = (event) => {
-    console.log('handlechange', event.target.value);
-    setValue(event.target.value);
-  };
-
-  const handleSubmitItem = (event) => {
-    event.preventDefault();
-    console.log('value', value);
-    addItem(value);
-    setValue('');
-  };
-
   return (
-    <form onSubmit={handleSubmitItem}>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        addItem(value);
+        setValue('');
+      }}
+    >
       <input
         type="text"
         value={value}
-        onChange={handleChange}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
         placeholder="New todo..."
         required
       />
