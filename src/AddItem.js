24d67f21a53c1b276f0c76226b54ccde2f77fcde
Richard Bristow <28766168+richardbristow@users.noreply.github.com>
2021-05-07 17:13:34 +0100

    moved additem css to styled component

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,12 +1,58 @@
-/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import './css/addItem.css';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+const StyledAddItem = styled.form`
+  display: flex;
+
+  input {
+    -webkit-transition: 0.5s;
+    transition: 0.5s;
+    margin: 0;
+    flex-basis: 0;
+    flex-grow: 9;
+    padding: 10px 15px;
+    font-size: 16px;
+    outline: none;
+    border: 3px solid #d3d7dd;
+    border-radius: 4px 0 0 4px;
+
+    &:focus {
+      border: 3px solid #b6bdc7;
+
+      & ~ button {
+        background-color: #b6bdc7;
+      }
+    }
+  }
+
+  button {
+    margin: 0;
+    flex-basis: 0;
+    flex-grow: 1;
+    padding: 10px 15px;
+    background: #d3d7dd;
+    border: 0;
+    border-radius: 0 4px 4px 0;
+    cursor: pointer;
+    outline: none;
+    -webkit-transition: 0.5s;
+    transition: 0.5s;
+
+    .fa-plus {
+      color: #2c3139;
+      font-size: 18px;
+    }
+  }
+`;
 
 const AddItem = ({ addItem }) => {
   const [value, setValue] = useState('');
 
   return (
-    <form
+    <StyledAddItem
       onSubmit={(event) => {
         event.preventDefault();
         addItem(value);
@@ -23,10 +69,14 @@ const AddItem = ({ addItem }) => {
         required
       />
       <button type="submit">
-        <span className="fa fa-plus" aria-hidden="true" />
+        <FontAwesomeIcon className="fa-plus" icon={faPlus} />
       </button>
-    </form>
+    </StyledAddItem>
   );
+};
+
+AddItem.propTypes = {
+  addItem: PropTypes.func.isRequired,
 };
 
 export default AddItem;
